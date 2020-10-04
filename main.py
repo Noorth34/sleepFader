51c05bad4bd7e7e8cb10b0cfe6fa45ce962df539
Gabriel Vidal <gabriel.vidal@gmx.fr>
2020-10-04 18:19:32 +0200

    create basic UI with play button

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #coding:utf-8
+
+import sys
+from sleepFaderDialog import *
+from PySide2.QtWidgets import QApplication
+
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	widget = SleepFaderDialogInstance()
+	widget.show()
+	app.exec_()
--- sleepFaderDialog.py
+++ sleepFaderDialog.py
@@ -1,1 +1,38 @@
 #coding:utf-8
+
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QStyle
+from PySide2.QtGui import QIcon
+from PySide2.phonon import Phonon
+
+class SleepFaderDialogInstance(QWidget):
+
+	def __init__(self):
+
+		super(SleepFaderDialogInstance, self).__init__()
+
+		self.setGeometry(600, 400, 200, 200)
+		self.setWindowTitle("Sleep Fader")
+
+		self._initUI()
+
+
+	def _initUI(self):
+
+		layout = QHBoxLayout(self)
+		self.playButton = QPushButton(self)
+		self.playButton.setGeometry(100, 50, 30, 30)
+		self.playButton.setIcon( self.style().standardIcon(QStyle.SP_MediaPlay) )
+		self.playButton.clicked.connect(self.playMedia)
+		layout.addWidget(self.playButton)
+
+
+	def _initMedia(self):
+		"""
+		"""
+		self.media =MediaObject(self)
+		self.media.setCurrentSource(MediaSource("/audio/trap.wav"))
+		audio =AudioOutput(MusicCategory, self)
+		path =createPath(media, audio)
+
+	def playMedia(self):
+		self.media.play()
